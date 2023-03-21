f6bc8a814564b67ab89325aefc841bf72575d4d4
TurekJakub <jakub.turek@student.gyarab.cz>
2023-03-21 09:22:49 +0100

    21/3/23

--- gamdb/movies/admin.py
+++ gamdb/movies/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from movies.models import Movies
+from movies.models import Movies,Director,Genre
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display=('name','year','get_genres')
 
 admin.site.register(Movies)
+admin.site.register(Director)
+admin.site.register(Genre)
 
 
 # Register your models here.
