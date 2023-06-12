6ce5d8c59f06b62042d8a271502d02e11eae7101
TurekJakub <jakub.turek@student.gyarab.cz>
2023-06-12 18:30:07 +0200

    UwU

--- gamdb/films/static/films/scripts/main.js
+++ gamdb/films/static/films/scripts/main.js
@@ -1,10 +1,12 @@
 function toggleMode() {
-  let icon = document.getElementById("icon");
+  let icon = document.getElementById("icon"); 
   icon.classList.toggle("bi-brightness-high-fill");
   icon.classList.toggle("bi-moon-stars-fill");
   if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
     document.documentElement.setAttribute("data-bs-theme", "light");
+    localStorage.setItem("theme", "light");
   } else {
     document.documentElement.setAttribute("data-bs-theme", "dark");
+    localStorage.setItem("theme", "dark");
   }
 }
