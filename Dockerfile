c1dea8ff044662c00adfc6022e54d2c56fa20b38
TurekJakub <jakub.turek@student.gyarab.cz>
2023-06-14 09:24:02 +0200

    Fix UwU

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,26 @@ FROM python:3.11
 
 RUN pip install --upgrade pip
 
-COPY ./requirements.txt .
-RUN pip install -r requirements.txt
-
-COPY ./gamdb /app
+COPY ./requirements.txt /
+RUN pip install --upgrade pip
+RUN pip install -r /requirements.txt
 
 ARG UID=1004
 ARG GID=1004
 
 RUN groupadd -g "${GID}" admin \
- && useradd --create-home --no-log-init -u "${UID}" -g "${GID}" admin
-
-WORKDIR /app
-
-USER admin
+    && useradd --create-home --no-log-init -u "${UID}" -g "${GID}" admin
 
 COPY ./start.sh /
-ENTRYPOINT ["sh","/start.sh"]
+
+WORKDIR /app
+USER admin
+
+EXPOSE 80
+VOLUME [ "/data", "/app" ]
+# STOPSIGNAL SIGQUIT  # replaced by die on term
+
+# TODO use start script
+# CMD uwsgi --http=0.0.0.0:80 --module=backend.wsgi --die-on-term --uid "${UID}" --gid "${GID}"
+ENTRYPOINT [ "/start.sh" ]
+CMD [ "uwsgi" ]
--- start.sh
+++ start.sh
@@ -1,5 +1,29 @@
- #!/bin/sh
- ./manage.py migrate --noinput
+#!/usr/bin/env bash
+set -e
+
+# TODO collectstatic
+# TODO compilemessages
+# TODO build sass/webpack
+
+# install any project requirements that are not installed in docker image
+#pip install -r requirements.txt
+
+# ./manage.py migrate --check
+# auto apply migrations - may be dangerous
  ./manage.py collectstatic --noinput
 
- exec uwsgi --http=0.0.0.0:8000 --module=gamdb.wsgi --die-on-term --touch-reload=/app/uwsgi.reload
+
+if [ "$1" = 'uwsgi' ]; then
+    # USER_ID=`id -u`
+    # GROUP_ID=`id -g`
+    # if [ ! -z "$2" ]; then
+    #     USER_ID="$2"
+    # fi
+    # if [ ! -z "$3" ]; then
+    #     GROUP_ID="$3"
+    # fi
+    # exec gosu admin uwsgi --http=0.0.0.0:80 --module=backend.wsgi --die-on-term --uid "${USER_ID}" --gid "${GROUP_ID}"
+    exec uwsgi --http=0.0.0.0:80 --module=gamdb.wsgi --die-on-term --touch-reload=/app/uwsgi.reload --check-static=/app/static
+fi
+
+exec "$@"
